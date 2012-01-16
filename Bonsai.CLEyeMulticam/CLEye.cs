02eeddb6d39f3073cadf36489c8c69c8acf11f11
glopes <glopesdev@users.noreply.github.com>
2012-01-16 18:21:16 +0000

    Added first revision of Bonsai.Packages repository.

---
+++ Bonsai.CLEyeMulticam/CLEye.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace Bonsai.CLEyeMulticam
+{
+    static class CLEye
+    {
+        const string libName = "CLEyeMulticam.dll";
+
+        [DllImport(libName, CallingConvention = CallingConvention.Cdecl)]
+        public static extern int CLEyeGetCameraCount();
+
+        [DllImport(libName, CallingConvention = CallingConvention.Cdecl)]
+        public static extern Guid CLEyeGetCameraUUID(int camId);
+
+        [DllImport(libName, CallingConvention = CallingConvention.Cdecl)]
+        public static extern IntPtr CLEyeCreateCamera(Guid camUUID, CLEyeCameraColorMode mode, CLEyeCameraResolution res, float frameRate);
+
+        [DllImport(libName, CallingConvention = CallingConvention.Cdecl)]
+        public static extern bool CLEyeDestroyCamera(IntPtr camera);
+
+        [DllImport(libName, CallingConvention = CallingConvention.Cdecl)]
+        public static extern bool CLEyeCameraStart(IntPtr camera);
+
+        [DllImport(libName, CallingConvention = CallingConvention.Cdecl)]
+        public static extern bool CLEyeCameraStop(IntPtr camera);
+
+        [DllImport(libName, CallingConvention = CallingConvention.Cdecl)]
+        public static extern bool CLEyeCameraLED(IntPtr camera, bool on);
+
+        [DllImport(libName, CallingConvention = CallingConvention.Cdecl)]
+        public static extern bool CLEyeSetCameraParameter(IntPtr camera, CLEyeCameraParameter param, int value);
+
+        [DllImport(libName, CallingConvention = CallingConvention.Cdecl)]
+        public static extern int CLEyeGetCameraParameter(IntPtr camera, CLEyeCameraParameter param);
+
+        [DllImport(libName, CallingConvention = CallingConvention.Cdecl)]
+        public static extern bool CLEyeCameraGetFrameDimensions(IntPtr camera, out int width, out int height);
+
+        [DllImport(libName, CallingConvention = CallingConvention.Cdecl)]
+        public static extern bool CLEyeCameraGetFrame(IntPtr camera, IntPtr pData, int waitTimeout);
+    }
+
+    // camera color mode
+    public enum CLEyeCameraColorMode
+    {
+        CLEYE_MONO_PROCESSED,
+        CLEYE_COLOR_PROCESSED,
+        CLEYE_MONO_RAW,
+        CLEYE_COLOR_RAW,
+        CLEYE_BAYER_RAW
+    };
+
+    // camera resolution
+    public enum CLEyeCameraResolution
+    {
+        CLEYE_QVGA,
+        CLEYE_VGA
+    };
+    // camera parameters
+    public enum CLEyeCameraParameter
+    {
+        // camera sensor parameters
+        CLEYE_AUTO_GAIN,			// [false, true]
+        CLEYE_GAIN,					// [0, 79]
+        CLEYE_AUTO_EXPOSURE,		// [false, true]
+        CLEYE_EXPOSURE,				// [0, 511]
+        CLEYE_AUTO_WHITEBALANCE,	// [false, true]
+        CLEYE_WHITEBALANCE_RED,		// [0, 255]
+        CLEYE_WHITEBALANCE_GREEN,	// [0, 255]
+        CLEYE_WHITEBALANCE_BLUE,	// [0, 255]
+        // camera linear transform parameters
+        CLEYE_HFLIP,				// [false, true]
+        CLEYE_VFLIP,				// [false, true]
+        CLEYE_HKEYSTONE,			// [-500, 500]
+        CLEYE_VKEYSTONE,			// [-500, 500]
+        CLEYE_XOFFSET,				// [-500, 500]
+        CLEYE_YOFFSET,				// [-500, 500]
+        CLEYE_ROTATION,				// [-500, 500]
+        CLEYE_ZOOM,					// [-500, 500]
+        // camera non-linear transform parameters
+        CLEYE_LENSCORRECTION1,		// [-500, 500]
+        CLEYE_LENSCORRECTION2,		// [-500, 500]
+        CLEYE_LENSCORRECTION3,		// [-500, 500]
+        CLEYE_LENSBRIGHTNESS		// [-500, 500]
+    };
+}
---
+++ Bonsai.CLEyeMulticam/CLEyeSource.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenCV.Net;
+using System.Threading;
+
+namespace Bonsai.CLEyeMulticam
+{
+    public class CLEyeSource : Source<IplImage>
+    {
+        IntPtr camera;
+        IplImage image;
+        IplImage output;
+        Thread captureThread;
+        volatile bool running;
+        ManualResetEventSlim stop;
+
+        public CLEyeSource()
+        {
+            ColorMode = CLEyeCameraColorMode.CLEYE_COLOR_RAW;
+            Resolution = CLEyeCameraResolution.CLEYE_VGA;
+            FrameRate = 60;
+        }
+
+        public int CameraIndex { get; set; }
+
+        public CLEyeCameraColorMode ColorMode { get; set; }
+
+        public CLEyeCameraResolution Resolution { get; set; }
+
+        public float FrameRate { get; set; }
+
+        protected override void Start()
+        {
+            captureThread = new Thread(CaptureNewFrame);
+            if (!CLEye.CLEyeCameraStart(camera))
+            {
+                throw new InvalidOperationException("Unable to start camera.");
+            }
+
+            running = true;
+            captureThread.Start();
+        }
+
+        protected override void Stop()
+        {
+            running = false;
+            stop.Wait();
+            CLEye.CLEyeCameraStop(camera);
+            captureThread = null;
+        }
+
+        public override IDisposable Load()
+        {
+            var guid = CLEye.CLEyeGetCameraUUID(CameraIndex);
+            if (guid == Guid.Empty)
+            {
+                throw new InvalidOperationException("No camera found with the given index.");
+            }
+
+            camera = CLEye.CLEyeCreateCamera(guid, ColorMode, Resolution, FrameRate);
+
+            int width, height;
+            CLEye.CLEyeCameraGetFrameDimensions(camera, out width, out height);
+
+            switch (ColorMode)
+            {
+                case CLEyeCameraColorMode.CLEYE_COLOR_RAW:
+                case CLEyeCameraColorMode.CLEYE_COLOR_PROCESSED:
+                    image = new IplImage(new CvSize(width, height), 8, 4);
+                    output = new IplImage(image.Size, 8, 3);
+                    break;
+                case CLEyeCameraColorMode.CLEYE_MONO_RAW:
+                case CLEyeCameraColorMode.CLEYE_MONO_PROCESSED:
+                    image = new IplImage(new CvSize(width, height), 8, 1);
+                    output = image;
+                    break;
+            }
+
+            stop = new ManualResetEventSlim();
+            return base.Load();
+        }
+
+        protected override void Unload()
+        {
+            CLEye.CLEyeDestroyCamera(camera);
+        }
+
+        void CaptureNewFrame()
+        {
+            while (running)
+            {
+                if (CLEye.CLEyeCameraGetFrame(camera, image.ImageData, 500))
+                {
+                    if (image.NumChannels == 4)
+                    {
+                        ImgProc.cvCvtColor(image, output, ColorConversion.BGRA2BGR);
+                    }
+                    Subject.OnNext(output);
+                }
+            }
+
+            stop.Set();
+        }
+    }
+}
